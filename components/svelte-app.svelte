5ed00a04d4a56004e58abca566528951a554f832
brandonxiang <1542453460@qq.com>
2021-07-04 14:03:42 +0800

    chore: update svelteup livereload

--- components/svelte-app.svelte
+++ components/svelte-app.svelte
@@ -6,7 +6,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial1</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
