20b168a7bb0312bab85e2a7d8cd24489dc0435e5
brandon.xiang <1542453460@qq.com>
2024-11-20 16:35:54 +0800

    add svelte-simple-router

---
+++ components/App.svelte
@@ -0,0 +1,34 @@
+<script>
+import { RouterView } from '@dvcol/svelte-simple-router/components';
+
+import HelloComponent from './hello/HelloComponent.svelte';
+import GoodbyeComponent from './goodbye/GoodbyeComponent.svelte';
+
+const RouteName = {
+  Hello: 'hello',
+  Goodbye: 'goodbye',
+  Home: 'home',
+  Any: 'any',
+} ;
+
+export const routes = [
+  {
+    name: RouteName.Home,
+    path: '/',
+    component: HelloComponent
+  },
+  {
+    name: RouteName.Goodbye,
+    path: `/goodbye`,
+    component: GoodbyeComponent
+  },
+];
+
+export const options = {
+  hash: true,
+  routes,
+};
+
+</script>
+
+<RouterView {options} />
